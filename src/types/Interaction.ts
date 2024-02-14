8c32337d00c404e3dd3fc7e7531208df3ae31977
Eren <65873936+Aello1@users.noreply.github.com>
2024-02-15 01:48:35 +0300

    Update Interaction.ts
    PublishType

--- src/types/Interaction.ts
+++ src/types/Interaction.ts
@@ -8,84 +8,49 @@ import { DBIMessageContextMenu } from "./other/MessageContextMenu";
 import { DBIModal } from "./Components/Modal";
 import { DBIStringSelectMenu } from "./Components/StringSelectMenu";
 import { DBIUserContextMenu } from "./other/UserContextMenu";
-
 export type TDBIInteractions<TNamespace extends NamespaceEnums> = DBIChatInput<TNamespace> | DBIButton<TNamespace> | DBIStringSelectMenu<TNamespace> | DBIMessageContextMenu<TNamespace> | DBIUserContextMenu<TNamespace> | DBIModal<TNamespace>;
-
 export interface IDBIBaseExecuteCtx<TNamespace extends NamespaceEnums> {
-  interaction:
-  | Discord.ChatInputCommandInteraction
-  | Discord.UserContextMenuCommandInteraction
-  | Discord.MessageContextMenuCommandInteraction
-  | Discord.ModalSubmitInteraction
-  | Discord.AutocompleteInteraction
-  | Discord.AnySelectMenuInteraction
-  | Discord.ButtonInteraction;
-  locale: {
-    user: DBILocale<TNamespace>,
-    guild?: DBILocale<TNamespace>
-  }
-  dbi: DBI<TNamespace>;
-  dbiInteraction: TDBIInteractions<TNamespace>;
-  setRateLimit(type: TDBIRateLimitTypes, duration: number): Promise<any>;
-  other: Record<string, any>;
-  clientNamespace: NamespaceData[TNamespace]["clientNamespaces"];
+    interaction: Discord.ChatInputCommandInteraction | Discord.UserContextMenuCommandInteraction | Discord.MessageContextMenuCommandInteraction | Discord.ModalSubmitInteraction | Discord.AutocompleteInteraction | Discord.AnySelectMenuInteraction | Discord.ButtonInteraction;
+    locale: {
+        user: DBILocale<TNamespace>;
+        guild?: DBILocale<TNamespace>;
+    };
+    dbi: DBI<TNamespace>;
+    dbiInteraction: TDBIInteractions<TNamespace>;
+    setRateLimit(type: TDBIRateLimitTypes, duration: number): Promise<any>;
+    other: Record<string, any>;
+    clientNamespace: NamespaceData[TNamespace]["clientNamespaces"];
 }
-
-export type TDBIReferencedData = ({ [key: string]: any, $ref: string, $unRef(): boolean } | string | number);
-
-export type TDBIInteractionTypes =
-  | "ChatInput"
-  | "UserContextMenu"
-  | "MessageContextMenu"
-  | "Modal"
-  | "Autocomplete"
-  | "StringSelectMenu"
-  | "UserSelectMenu"
-  | "ChannelSelectMenu"
-  | "MentionableSelectMenu"
-  | "RoleSelectMenu"
-  | "Button";
-
-export type TDBIRateLimitTypes =
-  | "User"
-  | "Channel"
-  | "Guild"
-  | "Member"
-  | "Message";
-
-
+export type TDBIReferencedData = ({
+    [key: string]: any;
+    $ref: string;
+    $unRef(): boolean;
+} | string | number);
+export type TDBIInteractionTypes = "ChatInput" | "UserContextMenu" | "MessageContextMenu" | "Modal" | "Autocomplete" | "StringSelectMenu" | "UserSelectMenu" | "ChannelSelectMenu" | "MentionableSelectMenu" | "RoleSelectMenu" | "Button";
+export type TDBIRateLimitTypes = "User" | "Channel" | "Guild" | "Member" | "Message";
 export type DBIRateLimit = {
-  type: TDBIRateLimitTypes;
-  /**
-   * Duration in milliseconds.
-   */
-  duration: number;
+    type: TDBIRateLimitTypes;
+    /**
+     * Duration in milliseconds.
+     */
+    duration: number;
+};
+export declare class DBIBaseInteraction<TNamespace extends NamespaceEnums> {
+    constructor(dbi: DBI<TNamespace>, cfg: Omit<DBIBaseInteraction<TNamespace>, "dbi">);
+    publish?: NamespaceData[TNamespace]["clientNamespaces"];
+    publishType?: "Global" | "Guild" | "None"
+    dbi: DBI<TNamespace>;
+    name: string;
+    description: string;
+    readonly type: TDBIInteractionTypes;
+    options?: any | any[];
+    other?: Record<string, any> & {
+        messageCommand?: {
+            aliases?: string[];
+        };
+    };
+    rateLimits?: DBIRateLimit[];
+    toJSON(overrides: any): any;
+    onExecute(ctx: IDBIBaseExecuteCtx<TNamespace>): Promise<void> | void;
 }
-
-export class DBIBaseInteraction<TNamespace extends NamespaceEnums> {
-  constructor(dbi: DBI<TNamespace>, cfg: Omit<DBIBaseInteraction<TNamespace>, "dbi">) {
-    this.dbi = dbi;
-    this.name = cfg.name;
-    this.description = cfg.description;
-    this.onExecute = cfg.onExecute;
-    this.type = cfg.type;
-    this.options = cfg.options;
-    this.other = cfg.other;
-    this.publish = cfg.publish ?? dbi.data.clients.first()?.namespace;
-    this.rateLimits = cfg.rateLimits ?? [];
-  }
-
-  publish?: NamespaceData[TNamespace]["clientNamespaces"];
-  dbi: DBI<TNamespace>;
-  name: string;
-  description: string;
-  readonly type: TDBIInteractionTypes;
-  options?: any | any[];
-  other?: Record<string, any> & { messageCommand?: { aliases?: string[] } };
-  rateLimits?: DBIRateLimit[];
-  toJSON(overrides: any): any { }
-
-  onExecute(ctx: IDBIBaseExecuteCtx<TNamespace>): Promise<void> | void {
-
-  }
-}
+//# sourceMappingURL=Interaction.d.ts.map
